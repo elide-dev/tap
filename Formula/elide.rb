eb0219c7a26daee0c1831f36f7bbb175e062c5de
Sam Gammon <sam@elide.ventures>
2024-05-15 04:07:54 -0700

    chore: updates for `alpha9` release
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- Formula/elide.rb
+++ Formula/elide.rb
@@ -1,11 +1,11 @@
 class Elide < Formula
   desc "Fast, polyglot JVM-based runtime"
   homepage "https://elide.dev"
-  version "1.0.0-alpha7"
+  version "1.0.0-alpha9"
   sha256 "1115002907d127f37dc8c819900aa432dd6438a9f0cc332a5e5f90550a213704"
   license "MIT"
   head "https://github.com/elide-dev/elide.git", using: :git
-  url "https://github.com/elide-dev/elide/archive/refs/tags/1.0.0-alpha7.tar.gz"
+  url "https://github.com/elide-dev/elide/archive/refs/tags/1.0.0-alpha9.tar.gz"
 
   depends_on "xz"
   uses_from_macos "zlib"
@@ -17,8 +17,14 @@ class Elide < Formula
   on_macos do
     on_arm do
       resource "elide_release" do
-        url "https://github.com/elide-dev/releases/releases/download/1.0.0-alpha7/elide.darwin-aarch64.txz"
-        sha256 "d6fc9ab7e189b9c755b5f488b45eb74596ef75d280d8c930d2914b06684ccc0b"
+        url "https://github.com/elide-dev/elide/releases/download/1.0.0-alpha9/elide-1.0.0-alpha9-darwin-aarch64.txz"
+        sha256 "7f758dfb3a429d0133ece5822e09a52b15e57d1d68eebb88de822844021ecabf"
+      end
+    end
+    on_amd64 do
+      resource "elide_release" do
+        url "https://github.com/elide-dev/elide/releases/download/1.0.0-alpha9/elide-1.0.0-alpha9-darwin-amd64.txz"
+        sha256 "f25e9199e74cd3f3870b8e2f87a559664cbf34e9196b1d6b0c9ab5b4bda858f4"
       end
     end
   end
@@ -26,8 +32,14 @@ class Elide < Formula
   on_linux do
     on_amd64 do
       resource "elide_release" do
-        url "https://github.com/elide-dev/releases/releases/download/1.0.0-alpha7/elide.linux-amd64.txz"
-        sha256 "f75a1d9ecb4c53d2041480d4ca8d329c893e3ad5c09023560b4d5e8b34d34e10"
+        url "https://github.com/elide-dev/elide/releases/download/1.0.0-alpha9/elide-1.0.0-alpha9-linux-amd64.tgz"
+        sha256 "6da4fc41250789475fc3b77d8485ac9aecc784b636bffe0f45b59613f5abda7c"
+      end
+    end
+    on_arm do
+      resource "elide_release" do
+        url "https://github.com/elide-dev/elide/releases/download/1.0.0-alpha9/elide-1.0.0-alpha9-linux-aarch64.tgz"
+        sha256 "19b826d80c6306aca410ca75eee2dea14dcb907b158e3fd0e1ee35d8b71e8c51"
       end
     end
   end
